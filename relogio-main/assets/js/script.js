afefcb1ee9e366a26573e7a6c96a6c8882ff8251
Mabiluz <160535329+Mabiluz@users.noreply.github.com>
2024-11-14 09:40:58 -0300

    pulse

--- relogio-main/assets/js/script.js
+++ relogio-main/assets/js/script.js
@@ -1,9 +1,19 @@
 function updateClock() {
     const now = new Date();
+    
+    // Obtém a hora e minutos
     const hours = String(now.getHours()).padStart(2, '0');
     const minutes = String(now.getMinutes()).padStart(2, '0');
     
-    document.getElementById('clock').innerHTML = `${hours}<span class="text-xl font-bold text-gray-50">:</span>${minutes}`;
+    // Atualiza o horário no elemento com ID "clock"
+    document.getElementById('clock').innerHTML = `${hours}<span class="colon">:</span>${minutes}`;
+
+    // Define o formato da data: dia da semana, dia e mês
+    const options = { weekday: 'short', day: 'numeric', month: 'short' };
+    const formattedDate = now.toLocaleDateString('pt-BR', options);
+
+    // Atualiza a data no elemento com a classe "date"
+    document.querySelector('.date').textContent = formattedDate;
 }
 
 // Atualiza o relógio imediatamente e depois a cada minuto
